76e0d6acac2120912652d004dc2c162f920bb2ea
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-11-18 16:18:49 +0100

    added set solutions

--- HashTables.py
+++ HashTables.py
@@ -85,9 +85,19 @@ def subarray_sum(nums, target):
         if current_sum == target:
             return [0, i]
         elif current_sum - target in sum_dict:
-            return [sum_dict[current_sum - target]+1, i]
+            return [sum_dict[current_sum - target] + 1, i]
         sum_dict[current_sum] = i
     return []
+
+
+def find_pairs(arr1, arr2, target):
+    pairs = []
+    set1 = set(arr1)
+    for num2 in arr2:
+        needed = target - num2
+        if needed in set1:
+            pairs.append((needed, num2))
+    return pairs
 
 
 print(two_sum([5, 1, 7, 2, 9, 3], 10))
@@ -104,3 +114,43 @@ print(find_duplicates([1, 3, 5, 2, 6, 9, 1, 2, 3, 6, 7, 9, 3]))
 print(group_anagrams(["listen", "silent", "triangle", "integral", "garden", "ranged"]))
 
 print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+
+# def has_unique_chars(string):
+#     new_string = sorted(string)
+#     for i in range(len(new_string) - 1):
+#         if new_string[i] == new_string[i + 1]:
+#             return False
+#     return True
+
+
+def has_unique_chars(string):
+    char_set = set()
+    for char in string:
+        if char in char_set:
+            return False
+        char_set.add(char)
+    return True
+
+
+print(has_unique_chars('abcdefg'))  # should return True
+print(has_unique_chars('hello'))  # should return False
+print(has_unique_chars(''))  # should return True
+print(has_unique_chars('0123456789'))  # should return True
+print(has_unique_chars('abacadaeaf'))  # should return False
+
+
+def longest_consecutive_sequence(nums):
+    set1 = set(nums)
+    max_length = 0
+
+    for num in set1:
+        if num - 1 not in set1:
+            current_num = num
+            current_length = 1
+
+            while current_num + 1 in set1:
+                current_num += 1
+                current_length += 1
+            max_length = max(max_length, current_length)
+    return max_length
